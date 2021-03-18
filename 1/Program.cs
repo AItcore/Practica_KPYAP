aef7fdff421551e4216cde8db4364cb8f8fc0b2c
Kirill Karpeev <kirillvip201@gmail.com>
2021-03-18 11:06:02 +0300

    новый файл:    1/Program.cs 	новый файл:    2/Program.cs 	новый файл:    3/Program.cs

---
+++ 1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void ForCycle()
+        {
+            System.Console.WriteLine("Цикл For");
+            for (double a = 1; a <= 3; a += 0.2)
+            {
+                System.Console.WriteLine((1 - Math.Pow(Math.Sin(a), 2)) / (1 + Math.Sin(a)));
+            }
+        }
+        static void WhileCycle()
+        {
+            System.Console.WriteLine("Цикл While");
+            double a = 1;
+            while (a < 3)
+            {
+                System.Console.WriteLine((1 - Math.Pow(Math.Sin(a), 2)) / (1 + Math.Sin(a)));
+                a += 0.2;
+            }
+        }
+        static void DoWhileCycle()
+        {
+            System.Console.WriteLine("Цикл Do While");
+            double a = 1;
+            do
+            {
+                System.Console.WriteLine((1 - Math.Pow(Math.Sin(a), 2)) / (1 + Math.Sin(a)));
+                a += 0.2;
+            }
+            while (a <= 3);
+        }
+        static void N2()
+        {
+            System.Console.WriteLine("Введите двузначное число: ");
+            string a = Console.ReadLine();
+            Random rnd = new Random();
+            int b = rnd.Next(10, 100);
+            if (int.Parse(a) > b)
+            {
+                System.Console.WriteLine($"Число {a} больше случайного числа {b} " + Math.Pow(int.Parse(a), 4));
+            }
+            else
+            {
+                System.Console.WriteLine($"Число {a} меньше случайного числа {b} " + int.Parse(a[0].ToString()) + int.Parse(a[1].ToString()));
+            }
+        }
+        static void N3()
+        {
+            Random rnd = new Random();
+            System.Console.WriteLine("Размер массива");
+            int N = int.Parse(Console.ReadLine());
+            int[,] arr = new int[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    arr[i,j] = rnd.Next(-100,100);
+                    Console.Write(arr[i,j] + " ");
+                }
+                System.Console.WriteLine();
+            }
+            int sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                sum += arr[i,i];
+            }
+            System.Console.WriteLine("Сумма главной диагонали: " + sum);
+        }
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Задание №1");
+            ForCycle();
+            WhileCycle();
+            DoWhileCycle();
+            System.Console.WriteLine("Задание №2");
+            N2();
+            System.Console.WriteLine("Задание №3");
+            N3();
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static double Integral(int n, double a, double x)
+        {
+            if (n > 1)
+            {
+                return (Math.Pow(x, n) * Math.Pow(Math.E, a * x)) / a - (n / a) * Integral(n - 1, a, x);
+            }
+            else if (n == 1)
+            {
+                return (Math.Pow(Math.E, a * x) / Math.Pow(a, 2)) * (a * x - 1);
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            double x = 0.2;
+            double a = 1.1;
+            double b = 0.04;
+            System.Console.WriteLine("Задание №1");
+            System.Console.WriteLine($"y = {Math.Pow(Math.Sin(Math.Pow(Math.Pow(x, 2) + a, 2)), 3) - Math.Sqrt(x / b)}");
+            System.Console.WriteLine($"z = {Math.Pow(x, 3) / 3 + Math.Cos(Math.Pow(x + b, 3))}");
+            System.Console.WriteLine();
+
+            Console.WriteLine("Задание 2");
+            Console.WriteLine("Пределы интегрирования:");
+            Console.WriteLine("Введите а");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите степень n");
+            int n = int.Parse(Console.ReadLine());
+            for (double i = a; i < b; i += 0.1)
+            {
+                Console.WriteLine($"Результат при i = {Math.Round(i, 1)}: {Integral(n, a, i)}");
+            }
+        }
+    }
+}
