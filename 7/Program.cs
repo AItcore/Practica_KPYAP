79a85f6f698b7aa27453f376060dce4dd840b29d
Kirill Karpeev <kirillvip201@gmail.com>
2021-03-25 10:00:31 +0300

    изменено:      7/Program.cs 	новый файл:    7/file.txt 	новый файл:    8/Program.cs 	новый файл:    8/input.txt 	новый файл:    8/output.txt 	новый файл:    9/Program.cs

--- 7/Program.cs
+++ 7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _7
 {
@@ -6,7 +7,38 @@ namespace _7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Задание №1");
+            string path = @"/home/kirill/coding/python/Practica_KPYAP/7/file.txt";
+            StreamReader reader = new StreamReader(path);
+            string[] text = reader.ReadToEnd().Split('\n');
+            System.Console.WriteLine("Содержимое файла:");
+            for (int i = 0; i < text.Length; i++)
+            {
+                System.Console.WriteLine(text[i]);
+            }
+            System.Console.WriteLine('\n');
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].IndexOf('?')!= -1)
+                System.Console.WriteLine(text[i]);
+            }
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].IndexOf('!')!= -1)
+                System.Console.WriteLine(text[i]);
+            }
+            System.Console.WriteLine("Задание №2");
+            System.Console.WriteLine("Введите текст");
+            text = Console.ReadLine().Split();
+            int min = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[min].Length > text[i].Length)
+                {
+                    min = i;
+                }
+            }
+            System.Console.WriteLine($"Порядковый номер слова с минимальной длиной {min+1}\nСимволов: {text[min].Length}\nСлово: {text[min]}");
         }
     }
 }
---
+++ 8/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _8
+{
+    enum CharacterType
+    {
+        Charismatic = 3,
+        Evil = -5,
+        HighlyСlassified = 7,
+        Fair = 4,
+        Dishonest = -3
+    }
+    struct Professor
+    {
+        public List<CharacterType> types;
+    }
+    struct Product : IComparable
+    {
+        public string type;
+        public int price;
+        public string variety;
+        public int count;
+        public int CompareTo(object obj)
+        {
+            Product otherProduct = (Product)obj;
+            return count.CompareTo(otherProduct.count);
+        }
+        public string ToString()
+        {
+            return $"{this.type}\n{this.price}\n{this.variety}\n{this.count}";
+        }
+    }
+    class Program
+    {
+        static void OptimalProfessor(Professor[] professors)
+        {
+            int[] arr = new int[professors.Length];
+            for (int i = 0; i < professors.Length; i++)
+            {
+                arr[i] = 0;
+                foreach (int item in professors[i].types)
+                {
+                    arr[i] += item;
+                }
+                System.Console.WriteLine($"Преподователь №{i + 1} баллов: {arr[i]}");
+            }
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[max] < arr[i])
+                {
+                    max = i;
+                }
+            }
+            System.Console.WriteLine($"Лучший преподователь №{max} баллов: {arr[max]}");
+        }
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Задание №1");
+            System.Console.Write("Количество преподователей: ");
+            Professor[] professors = new Professor[int.Parse(Console.ReadLine())];
+            for (int i = 0; i < professors.Length; i++)
+            {
+                professors[i] = new Professor();
+                professors[i].types = new List<CharacterType>();
+                int choose = 0;
+                bool work = true;
+                while (work)
+                {
+                    System.Console.WriteLine("0 - След. преподаватель 1 - Харизматичный 2 - Злой 3 - Высоко классифицированный 4 - Честный 5 - Не честный");
+                    choose = int.Parse(Console.ReadLine());
+                    CharacterType type = CharacterType.Charismatic;
+                    switch (choose)
+                    {
+                        case 0:
+                            work = false;
+                            break;
+                        case 1:
+                            professors[i].types.Add(CharacterType.Charismatic);
+                            break;
+                        case 2:
+                            professors[i].types.Add(CharacterType.Evil);
+                            break;
+                        case 3:
+                            professors[i].types.Add(CharacterType.HighlyСlassified);
+                            break;
+                        case 4:
+                            professors[i].types.Add(CharacterType.Fair);
+                            break;
+                        case 5:
+                            professors[i].types.Add(CharacterType.Dishonest);
+                            break;
+                    }
+                }
+            }
+            OptimalProfessor(professors);
+            // string path = "/home/kirill/coding/python/Practica_KPYAP/8/";
+            // StreamReader reader = new StreamReader(path + "input.txt");
+            // StreamWriter writer = new StreamWriter(path + "output.txt");
+            // Product[] products = new Product[3];
+            // int i = 0;
+            // while (!(reader.Peek() == -1))
+            // {
+            //     products[i].type = reader.ReadLine();
+            //     products[i].price = int.Parse(reader.ReadLine());
+            //     products[i].variety = reader.ReadLine();
+            //     products[i].count = int.Parse(reader.ReadLine());
+            //     i++;
+            // }
+            // Array.Sort(products);
+            // System.Console.WriteLine("Введите максимальное количество продуктов");
+            // int a = int.Parse(Console.ReadLine());
+            // for (i = 0; i < products.Length; i++)
+            // {
+            //     if (products[i].count < a )
+            //     {
+            //         writer.WriteLine(products[i].ToString());
+            //     }
+            // }
+            // writer.Close();
+            // reader.Close();
+        }
+    }
+}
